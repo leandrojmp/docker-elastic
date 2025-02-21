aa6c1da08a13df5ecc694fcd65b3956ff5a95497
Leandro Maciel <leandrojmp@gmail.com>
2025-02-21 10:59:45 -0300

    ajuste clean-up.sh

--- certs/clean-up.sh
+++ certs/clean-up.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 echo "remove certificados"
-rm ca-siem-key.pem
-rm ca-siem.pem
-rm siem-key.pem
-rm siem.pem
-rm siem.chain.pem
-rm certs.exist
+rm -f ca-siem-key.pem
+rm -f ca-siem.pem
+rm -f siem-key.pem
+rm -f siem.pem
+rm -f siem.chain.pem
+rm -f certs.exist
 
 echo "remove locks do setup do fleet e kibana"
-rm fleet_setup.exist
-rm kibana_user.exist
+rm -f fleet_setup.exist
+rm -f kibana_user.exist
 
